36567e95b6e51fadcfa41da436eaf47a5dcde816
animeshprasad15 <56110479+animeshprasad15@users.noreply.github.com>
2019-10-13 23:40:05 +0530

    updated armstrong function

--- Armstrong_numbers.c
+++ Armstrong_numbers.c
@@ -1,12 +1,26 @@
 #include<stdio.h>
+#include<math.h>
+
+int countDigits(int x)
+{
+    int c=0;
+    while(x)
+    {
+        x/=10;
+        c++;
+    }
+    return c;
+}
+
 void fun(int n)
 {
     int sum=0,r,l;
     l=n;
+    int ex=countDigits(n);
     while(l>0)
     {
         r=l%10;
-        sum=sum+r*r*r;
+        sum+=pow(r,ex);    //an armstrong number is a n digit number (say x) whose digits when raised to power n (and not 3 always as you did) and summed equals x.
         l=l/10;
     }
     if(sum==n)
@@ -15,8 +29,8 @@ void fun(int n)
 int main()
 {
     int n;
-    printf("Enter a number :");    //Find the Armstrong numbers upto n number
-    scanf("%d",&n);
+    printf("Enter a number :");    //Find the Armstrong numbers upto n number (you can now enter values even greater than 999)
+    scanf("%d",&n);               
     for(int i=1;i<=n;i++)
         fun(i);
 }
